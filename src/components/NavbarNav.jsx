196f8c1a3e461c960893a7062de229a20eeed076
Kareem Aboelnaga <kaaboelnaga@gmail.com>
2025-05-22 04:57:13 +0300

    Final changes to navbar (hopefully)

--- src/components/NavbarNav.jsx
+++ src/components/NavbarNav.jsx
@@ -60,11 +60,11 @@ export default function NavbarNav(){
         <Navbar.Brand as={Link} to={'/'} className="mx-5 inter-700">{navbar.navTitle[language]}</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="justify-content-between align-items-center w-100">
-            <Nav className="d-lg-align-items-center">
+            <Nav className="d-lg-flex align-items-lg-center">
                 {location.pathname === '/' &&
             <>            
-                <Nav.Item onClick={() => changeCategory('movies')} className={`mx-3 my-2 ${category === 'movies' ? 'inter-700 text-decoration-underline' : 'inter-400'}`}  style={{ cursor: 'pointer' }}>{navbar.navMovies[language]}</Nav.Item>
-                <Nav.Item onClick={() => changeCategory('shows')} className={`mx-3 my-2 ${category === 'shows' ? 'inter-700 text-decoration-underline' : 'inter-400'}`}  style={{ cursor: 'pointer' }}>{navbar.navShows[language]}</Nav.Item>
+                <Nav.Item onClick={() => changeCategory('movies')} className={`mx-3 my-2 ${category === 'movies' ? 'inter-700 fs-4 text-decoration-underline' : 'inter-400'}`}  style={{ cursor: 'pointer' }}>{navbar.navMovies[language]}</Nav.Item>
+                <Nav.Item onClick={() => changeCategory('shows')} className={`mx-3 my-2 ${category === 'shows' ? 'inter-700 fs-4 transition duration-500' : 'inter-400'}`}  style={{ cursor: 'pointer' }}>{navbar.navShows[language]}</Nav.Item>
             </>
 
         }
@@ -77,7 +77,7 @@ export default function NavbarNav(){
                 <NavDropdown.Item eventKey="zh"active={language === 'zh'}>ZH</NavDropdown.Item>
             </NavDropdown>
             <span className="nav-item align-items-center justify-content-end">
-                <Link className="nav-item p-0" to="/watchlist">
+                <Link className="nav-link p-0" to="/watchlist">
                     <button className="nav-item btn border-0"><i className="bi bi-heart-fill fs-3"></i>{navbar.watchList[language]}</button>
                     <span className="d-inline-flex align-items-start bg-light2 px-2 rounded-5 me-1" style={{fontSize: '15px'}}>{Object.keys(favoriteItems).length > 0 && Object.keys(favoriteItems).length}</span>
                 </Link>
