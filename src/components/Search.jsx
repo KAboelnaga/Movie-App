5bd5325351233af5ce5f0bf3fc700ef51523ff1c
Kareem Aboelnaga <kaaboelnaga@gmail.com>
2025-05-23 04:36:54 +0300

    downloaded and tried motion on search icon

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useRef, useState } from "react"
 import { useNavigate } from "react-router";
 import { LanguageContext } from "../context/LanguageContext";
 import searchItems from "./JS/search";
+import { motion } from "motion/react";
 export default function Search({searchValue}){
     const emptySearch = useRef(null);
     const [search,setSearch] = useState('');
@@ -31,7 +32,8 @@ export default function Search({searchValue}){
         <>
         <form className="d-flex align-items-center py-3 mt-5 flex-wrap gap-2 w-100" style={{width: '80vw'}}>
             <input type="search" name="search" id="search" placeholder={searchItems.placeHolder[language]} value={search} className="bg-white text-black border-1 border-light2 rounded-2 flex-grow-1 mx-3 me-3" style={{height:'5vh'}} onChange={handleChange} />  
-            <button className="btn btn-yellow mx-3" onClick={handleSearch}>{searchItems.searchButton[language]}</button>
+            <motion.button whileHover={{ scale: 1.1 }}
+  whileTap={{ scale: 0.95 }} initial={{ x: 100}} animate={{ x: 0 , transition: {duration: 0.5}}} className="btn btn-yellow mx-3" onClick={handleSearch}>{searchItems.searchButton[language]}</motion.button>
         </form>
         <h4 className="text-danger inter-500 d-none " ref={emptySearch}>Search is empty!</h4>
         </>
